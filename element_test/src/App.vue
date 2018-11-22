32d469e2417844a1608a4874fb61b1b7ea4ff397
LLS <2145604059@qq.com>
2018-11-22 09:38:50 +0800

    使用element UI

---
+++ element_test/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+	<el-container>
+		<el-aside width="200px">Aside</el-aside>
+		<el-container>
+			<el-header>Header</el-header>
+			<el-main>
+				<el-carousel indicator-position="outside">
+					<el-carousel-item v-for="item in 4" :key="item">
+						<h3>{{ item }}</h3>
+					</el-carousel-item>
+				</el-carousel>
+			</el-main>
+		</el-container>
+	</el-container>
+
+</template>
+
+<script>
+	export default {
+
+
+	}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	.el-header {
+		background-color: #B3C0D1;
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
+
+	.el-aside {
+		background-color: #D3DCE6;
+		color: #333;
+		text-align: center;
+		line-height: 600px;
+	}
+
+	.el-main {
+		background-color: #E9EEF3;
+		color: #333;
+		text-align: center;
+		line-height: 600px;
+	}
+
+	body>.el-container {
+		margin-bottom: 40px;
+	}
+
+	.el-container:nth-child(5) .el-aside,
+	.el-container:nth-child(6) .el-aside {
+		line-height: 260px;
+	}
+
+	.el-container:nth-child(7) .el-aside {
+		line-height: 320px;
+	}
+
+	.el-carousel__item h3 {
+		color: #475669;
+		font-size: 18px;
+		opacity: 0.75;
+		line-height: 300px;
+		margin: 0;
+	}
+
+	.el-carousel__item:nth-child(2n) {
+		background-color: #99a9bf;
+	}
+
+	.el-carousel__item:nth-child(2n+1) {
+		background-color: #d3dce6;
+	}
+</style>
